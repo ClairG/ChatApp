3ede9847bd7e31c33b1958008ce786defecda236
lemon <lemon>
2019-04-24 16:44:35 -0400

    1.[CHG] add UserProfile

--- ClairG.ChatApp/ClairG.ChatApp.Domain/Entities/User.cs
+++ ClairG.ChatApp/ClairG.ChatApp.Domain/Entities/User.cs
@@ -23,6 +23,7 @@ namespace ClairG.ChatApp.Domain.Entities
         public string Email { get; set; }
 
         [ScaffoldColumn(false)]
+        [Display(Name ="Picture")]
         public string ImageUrl { get; set; }
 
         [ScaffoldColumn(false)]
---
+++ ClairG.ChatApp/ClairG.ChatApp.WebApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using ClairG.ChatApp.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClairG.ChatApp.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private EFDbContext db = new EFDbContext();
+
+        //User/UserProfile
+        public ActionResult UserProfile()
+        {
+            int userId = Convert.ToInt32(Session["UserId"]);
+            if (userId == 0)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            //if user is looged in
+            return View(db.Users.Find(userId));
+        }
+    }
+}
