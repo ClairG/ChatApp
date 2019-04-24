d271a1e5fd7bb446bb36a62315f30dca22c93056
lemon <lemon>
2019-04-24 13:11:06 -0400

    1.[CHG]add ChatRoom

--- ClairG.ChatApp/ClairG.ChatApp.WebApp/Controllers/ChatRoomController.cs
+++ ClairG.ChatApp/ClairG.ChatApp.WebApp/Controllers/ChatRoomController.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using ClairG.ChatApp.Domain.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace ClairG.ChatApp.WebApp.Controllers
 {
     public class ChatRoomController : Controller
     {
+        private EFDbContext db = new EFDbContext();
         // GET: ChatRoom
         public ActionResult Index()
         {
-            return View();
+            var comments = db.Comments.Include(x => x.Replies).ToList();
+            return View(comments);
         }
     }
 }
