6651af7bcc9844494d0d3c243b8fb9c5a96dce1e
lemon <lemon>
2019-04-24 13:48:12 -0400

    1.[CHG] add function of Reply in ChatRoom

--- ClairG.ChatApp/ClairG.ChatApp.WebApp/Controllers/ChatRoomController.cs
+++ ClairG.ChatApp/ClairG.ChatApp.WebApp/Controllers/ChatRoomController.cs
@@ -5,17 +5,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using ClairG.ChatApp.WebApp.Models;
+using ClairG.ChatApp.Domain.Entities;
 
 namespace ClairG.ChatApp.WebApp.Controllers
 {
     public class ChatRoomController : Controller
     {
         private EFDbContext db = new EFDbContext();
-        // GET: ChatRoom
+
+        //ChatRoom/Index
         public ActionResult Index()
         {
             var comments = db.Comments.Include(x => x.Replies).ToList();
             return View(comments);
+        }
+
+        //PostReply
+        public ActionResult PostReply(ReplyVM obj)
+        {
+            Reply r = new Reply();
+            r.CommentId = obj.CommendId;
+            r.Text = obj.Reply;
+
+            db.Replies.Add(r);
+            db.SaveChanges();
+
+            return View("Index");
         }
     }
 }
---
+++ ClairG.ChatApp/ClairG.ChatApp.WebApp/Models/ReplyVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClairG.ChatApp.WebApp.Models
+{
+    public class ReplyVM
+    {
+        [Required]
+        public string Reply { get; set; } //<input type="text" name="Reply" />
+
+        public int CommendId { get; set; } //<input type="hidden" name="CommendId" value="@comment.Id"/>
+    }
+}
